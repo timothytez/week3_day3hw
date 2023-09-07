017d8ec6e306b53d6ae494dcd1285b6fd7ad231d
Timothy Crooms <timothy_crooms@yahoo.com>
2023-09-07 09:15:40 -0400

    week3 hw3

---
+++ whiteboard/wb.py
@@ -0,0 +1,8 @@
+# On vacation in the Bahamas you decide to go out on a scuba trip one day.
+
+# Unfortunately, disaster has struck!! The boat has caught fire!!
+
+# You will be provided a string that lists many boat related items. Make a function called replace_fire that takes in a string (input) and if any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". Then return the string .
+
+# input: "Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast",
+# output: "Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast"
